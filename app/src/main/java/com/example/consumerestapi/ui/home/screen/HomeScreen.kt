510cece314fbd83a3e3e96482ed7aec850861724
TA1028 <dina.wulan.ft21@mail.umy.ac.id>
2023-12-27 20:40:37 +0700

    Menambahkan parameter onDeleteClick

--- app/src/main/java/com/example/consumerestapi/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/com/example/consumerestapi/ui/home/screen/HomeScreen.kt
@@ -86,6 +86,7 @@ fun KontakLayout(kontak: List<Kontak>, modifier: Modifier = Modifier){
 @Composable
 fun KontakCard(
     kontak: Kontak,
+    onDeleteClick: (Kontak) -> Unit = {},
     modifier: Modifier = Modifier
 ) {
     Card(
