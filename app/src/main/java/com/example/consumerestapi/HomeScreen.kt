7b4029a0799f38d64173da355555f81ee5f489e2
TA1028 <dina.wulan.ft21@mail.umy.ac.id>
2023-12-28 08:06:36 +0700

    Menambahkan object DestinasiHome dan mengimplementasikan antarmuka Destinasi Navigasi

--- app/src/main/java/com/example/consumerestapi/HomeScreen.kt
+++ app/src/main/java/com/example/consumerestapi/HomeScreen.kt
@@ -1,3 +1,10 @@
 package com.example.consumerestapi
 
+import com.example.consumerestapi.navigation.DestinasiNavigasi
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route : String = "home"
+    override val titleRes : String = "Kontak"
+}
+
 sealed class HomeScreen
