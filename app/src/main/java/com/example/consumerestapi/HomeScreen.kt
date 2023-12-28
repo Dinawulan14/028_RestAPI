67e66cb354d978d954cf0760f22fadd6ea5abc14
TA1028 <dina.wulan.ft21@mail.umy.ac.id>
2023-12-28 08:09:25 +0700

    Membuat function HomeScreen

--- app/src/main/java/com/example/consumerestapi/HomeScreen.kt
+++ app/src/main/java/com/example/consumerestapi/HomeScreen.kt
@@ -1,10 +1,26 @@
 package com.example.consumerestapi
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.consumerestapi.navigation.DestinasiNavigasi
+import com.example.consumerestapi.ui.PenyediaViewModel
+import com.example.consumerestapi.ui.home.viewmodel.HomeViewModel
 
 object DestinasiHome : DestinasiNavigasi {
     override val route : String = "home"
     override val titleRes : String = "Kontak"
 }
 
-sealed class HomeScreen
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+    navigateToItemEntry: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDetailClick: (Int) -> Unit ={},
+    viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+
+}
+
