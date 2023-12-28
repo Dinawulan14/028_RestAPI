066e7245c90f605fd485af388ec3531d5c7ffbdb
TA1028 <dina.wulan.ft21@mail.umy.ac.id>
2023-12-28 07:57:24 +0700

    Menambahkan fungsi clickable

--- app/src/main/java/com/example/consumerestapi/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/com/example/consumerestapi/ui/home/screen/HomeScreen.kt
@@ -1,6 +1,7 @@
 package com.example.consumerestapi.ui.home.screen
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -94,8 +95,14 @@ fun KontakLayout(
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ){
         items(kontak){kontak ->
-            KontakCard(kontak = kontak
-                , modifier = Modifier.fillMaxWidth()
+            KontakCard(
+                kontak = kontak,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable { onDetailClick(kontak) },
+                onDeleteClick = {
+                    onDeleteClick(kontak)
+                }
             )
         }
     }
